c171a8a09934152d2c54f437e2c9c9b08e04cc85
makel <makellum@gmail.com>
2020-09-05 14:40:33 -0400

    “update”

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 //Timer JS code
 // Set the date we're counting down to
 var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
@@ -7,24 +8,71 @@ var x = setInterval(function() {
 
   // Get today's date and time
   var now = new Date().getTime();
-    
+
   // Find the distance between now and the count down date
   var distance = countDownDate - now;
-    
+
   // Time calculations for days, hours, minutes and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
+
   // Output the result in an element with id="demo"
   document.getElementById("demo").innerHTML = hours + "h "
+
   + minutes + "m " + seconds + "s ";
-    
+
   // If the count down is over, write some text 
   if (distance < 0) {
+
     clearInterval(x);
+
     document.getElementById("demo").innerHTML = "EXPIRED";
   }
 }, 1000);
 //End of Timer JS code
+var website = $("#website-input").val();
+
+console.log(website)
+
+var queryURL = "http://api.thegreenwebfoundation.org/greencheck/{url}"
+
+$.ajax({
+
+    url: queryURL,
+
+    method: "GET"
+
+}).then(function(response) {
+
+    $(".green").text("Is It Green: " + response.green);
+
+    $(".url").text("URL: " + response.url);
+
+    $(".hostedby").text("Host: " + response.hostedby);
+
+    $(".hostedbywebsite").text("Host Website: " + response.hostedbywebsite);
+    
+
+    var green = ("this site is" + response.green);
+
+    var url = ("The sites url is" + response.url);
+
+    var hostedby = ("Host: " + response.hostedby);
+
+    var hostedbywebsite = ("Host Website: " + response.hostedbywebsite)
+})
+
+
+
+// {"green":true,
+// "url":"google.com",
+// "data":true,
+// "hostedby":"Google Inc.",
+// "hostedbyid":595,
+// "hostedbywebsite":"www.google.com",
+// "partner":null}
